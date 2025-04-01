3517649e63a662e421536e1a292abb284acab7df
Trịnh Đồng <127869685+trinhdong255@users.noreply.github.com>
2025-04-01 20:40:56 +0700

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,7 @@ import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(process.env.MONGO_URI || 'mongodb://localhost/fashion-sales-chatbot'),
+    MongooseModule.forRoot(process.env.MONGO_URI || 'mongodb://localhost:27017/fashion-sales-chatbot'),
     ProductsModule,
   ],
   controllers: [AppController],
